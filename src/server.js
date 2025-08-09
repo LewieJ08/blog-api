417e6c0369525295f07a307d9090ccef781e4f4c
LewieJ08 <lewiej08@gmail.com>
2025-08-09 17:36:51 +0100

    refactor: delete index route '/'

--- src/server.js
+++ src/server.js
@@ -8,10 +8,6 @@ app.use(express.json());
 app.use(loggerMiddleware)
 app.use("/articles", articlesRouter)
 
-app.get("/", (req, res) => {
-    res.send("Here")
-});
-
 app.listen(3000, () => {
     console.log("Server running on port: 3000")
 });
