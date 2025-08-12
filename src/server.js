5fe72f33324309ec91ba8fc1c594a6ea5c052e79
LewieJ08 <lewiej08@gmail.com>
2025-08-12 22:34:42 +0100

    refactor: update articles.js file name

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const loggerMiddleware = require("./middleware/logger");
-const articlesRouter = require("./routes/articles");
+const articlesRouter = require("./routes/articlesRoutes");
 
 const app = express();
 require('dotenv').config({path: __dirname + "/.env"});
