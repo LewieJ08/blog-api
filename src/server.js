8ebf4d19b45b6b7488caf5987c84ce31ba6de39f
LewieJ08 <lewiej08@gmail.com>
2025-08-12 22:12:46 +0100

    feat: import connectDB func into app entry point

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
-const app = express();
-require('dotenv').config({path: __dirname + "/.env"});
-
+const connectDB = require("./config/db");
 const loggerMiddleware = require("./middleware/logger");
 const articlesRouter = require("./routes/articles");
 
+const app = express();
+require('dotenv').config({path: __dirname + "/.env"});
+connectDB();
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-app.use(loggerMiddleware)
-app.use("/articles", articlesRouter)
+app.use(loggerMiddleware);
+app.use("/articles", articlesRouter);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port: ${process.env.PORT}`)
+    console.log(`Server running on port: ${process.env.PORT}`);
 });
