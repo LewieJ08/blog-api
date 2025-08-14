cdcabf1ea4f2f46364bc5c73807f789514c194c5
LewieJ08 <lewiej08@gmail.com>
2025-08-14 10:11:37 +0100

    feat: import controller

--- src/routes/articlesRoutes.js
+++ src/routes/articlesRoutes.js
@@ -1,23 +1,15 @@
 const express = require("express");
+const createArticle = require("../controllers/articlesController");
+
 const router = express.Router();
 
 router.route("/")
-    .get( (req, res) => {
-        res.send("Get all articles");
-    })
-    .post( (req, res) => {
-        res.send("Create New Post");
-    });
+    .get(() => {})
+    .post(createArticle);
 
 router.route("/:id")
-    .get((req, res) => {
-        res.send(`Get article with ID: ${req.params.id}`);
-    })
-    .put((req, res) => {
-        res.send(`Update article with ID: ${req.params.id}`);
-    })
-    .delete ((req, res) => {
-        res.send(`Delete article with ID: ${req.params.id}`);
-    });
+    .get(() => {})
+    .put(() => {})
+    .delete (() => {});
 
 module.exports = router;
