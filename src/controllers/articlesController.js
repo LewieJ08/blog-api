553d779b9e8a0d82a6c87f0a798bded0ab830fb4
LewieJ08 <lewiej08@gmail.com>
2025-08-15 15:46:46 +0100

    feat: update and improve createArticle function within controller

--- src/controllers/articlesController.js
+++ src/controllers/articlesController.js
@@ -1,9 +1,17 @@
 const Article = require("../models/articles");
 
-const createArticle = async (req, res) => {
-    const article = new Article(req.body);
-    await article.save();
-    res.json({message: "Article Created"});
+const createArticle = async (req, res, next) => {
+    try {
+        const article = new Article({...req.body, createdAt: new Date()});
+        await article.save();
+        res.status(201).json({
+            success: true,
+            message: "Article successfully created",
+            data: article
+        });
+    } catch(error) {
+        next(error);
+    }
 };
 
 module.exports = createArticle;
